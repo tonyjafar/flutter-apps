83601b92aa15bf4b1074fd7cb114a22206c5ccfb
tony <thaer.m.jafar@gmail.com>
2023-01-15 17:54:12 +0100

    using Cloumn widget

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -5,12 +5,32 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'what\'s your favorite color?',
+      'what\'s you faorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('Text!!!'),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
