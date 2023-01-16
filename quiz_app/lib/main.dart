05f3a45657273f95735e69857982517d15ce1cf9
tony <thaer.m.jafar@gmail.com>
2023-01-16 14:23:06 +0100

    change questions to map

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -14,10 +14,35 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var questions = [
-    'what\'s your favorite color?',
-    'what\'s your favorite animal?'
+    {
+      'questionText': 'what\'s your favorite color?',
+      'answers': [
+        'balck',
+        'red',
+        'green',
+        'white',
+      ],
+    },
+    {
+      'questionText': 'what\'s your favorite animal',
+      'answers': [
+        'dog',
+        'cat',
+        'rabbit',
+        'cow',
+      ],
+    },
+    {
+      'questionText': 'what\'s your favorite City',
+      'answers': [
+        'Hamburg',
+        'Munich',
+        'London',
+        'Berlin',
+      ],
+    },
   ];
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
       if (_questionIndex >= questions.length - 1) {
         _questionIndex = 0;
@@ -37,20 +62,12 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(
-              questions[_questionIndex],
+              questions[_questionIndex]['questionText'],
             ),
-            Answer(
-              answerQuestion,
-              'Answer 1',
-            ),
-            Answer(
-              answerQuestion,
-              'Answer 2',
-            ),
-            Answer(
-              answerQuestion,
-              'Answer 3',
-            ),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList(),
           ],
         ),
       ),
