7cfd7712ffc4d11ccb645df5ffed4e9cf335b32f
tony <thaer.m.jafar@gmail.com>
2023-01-16 13:04:39 +0100

    add some styles

--- quiz_app/lib/question.dart
+++ quiz_app/lib/question.dart
@@ -5,7 +5,16 @@ class Question extends StatelessWidget {
   Question(this.questionText);
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(20),
+      child: Text(
+        questionText,
+        style: TextStyle(
+          fontSize: 25,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
-
 }
