6e03c1b97989491c0823c784460bdd6755e396bf
tony <thaer.m.jafar@gmail.com>
2023-01-23 17:06:05 +0100

    Revert "testing"
    This reverts commit 80236696df5c2b7bddc57aa71beb576482c8701c.

--- personal_expenses/lib/main.dart
+++ personal_expenses/lib/main.dart
@@ -1,58 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:personal_expenses/widgets/add_transaction.dart';
-import 'widgets/expenses.dart';
+import 'widgets/user_transaction.dart';
 import 'widgets/charts.dart';
-import 'package:personal_expenses/models/transaction.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
-  State<StatefulWidget> createState() {
-    return MyAppHome();
+  Widget build(BuildContext context) {
+    return const MyAppHome();
   }
 }
 
-class MyAppHome extends State<MyApp> {
-  void _addTransaction(String txTitle, double txAmount) {
-    final newTx = Transaction(
-      id: DateTime.now().toString(),
-      title: txTitle,
-      amount: txAmount,
-      date: DateTime.now(),
-    );
-    setState(() {
-      _transactions.add(newTx);
-    });
-  }
-
-  final List<Transaction> _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 99.55,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Retaurant',
-      amount: 23.55,
-      date: DateTime.now(),
-    ),
-  ];
-  void startAddNewTransaction(BuildContext ctx) {
-    showModalBottomSheet(
-      context: ctx,
-      builder: (_) {
-        return GestureDetector(
-          onTap: () {},
-          child: AddTransaction(_addTransaction),
-        );
-      },
-    );
-  }
-
+class MyAppHome extends StatelessWidget {
+  const MyAppHome({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -60,27 +21,12 @@ class MyAppHome extends State<MyApp> {
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
           title: const Text("Personal Expenses"),
-          actions: [
-            IconButton(
-              onPressed: () => startAddNewTransaction(context),
-              icon: const Icon(
-                Icons.add,
-              ),
-            )
-          ],
         ),
         body: Column(
-          children: [
-            const Charts(),
-            Expenses(_transactions),
+          children: const [
+            Charts(),
+            UserTransaction(),
           ],
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(
-            Icons.add,
-          ),
-          onPressed: () => startAddNewTransaction(context),
         ),
       ),
     );
--- personal_expenses/lib/widgets/user_transaction.dart
+++ personal_expenses/lib/widgets/user_transaction.dart
@@ -1,1 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:personal_expenses/widgets/add_transaction.dart';
+import 'package:personal_expenses/widgets/expenses.dart';
+import 'package:personal_expenses/models/transaction.dart';
 
+class UserTransaction extends StatefulWidget {
+  const UserTransaction({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _UserTransactionState();
+  }
+}
+
+class _UserTransactionState extends State<UserTransaction> {
+  final List<Transaction> _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 99.55,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Retaurant',
+      amount: 23.55,
+      date: DateTime.now(),
+    ),
+  ];
+  void _addTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      id: DateTime.now().toString(),
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+    );
+    setState(() {
+      _transactions.add(newTx);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        AddTransaction(_addTransaction),
+        Expenses(_transactions),
+      ],
+    );
+  }
+}
