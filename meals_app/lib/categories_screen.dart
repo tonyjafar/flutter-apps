23af5b1743ad601d111678c0b8f7cb3dd19608bc
tony <thaer.m.jafar@gmail.com>
2023-02-08 14:06:15 +0100

    start with meals app

---
+++ meals_app/lib/categories_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'dummy_data.dart';
+import 'category_item.dart';
+
+class GatecoriesScreen extends StatelessWidget {
+  const GatecoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("TonyMeals"),
+        ),
+        body: GridView(
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 1.5,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: dummyCatageroy
+              .map(
+                (cat) => CategoryItem(
+                  cat.title,
+                  cat.color,
+                ),
+              )
+              .toList(),
+        ));
+  }
+}
