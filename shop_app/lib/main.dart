31afdfcb45ac58fb4f44bd074a1d0f7aae424a22
tony <thaer.m.jafar@gmail.com>
2023-03-02 12:24:09 +0100

    start shop app

---
+++ shop_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/products_overview_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductOverviewScreen(),
+    );
+  }
+}
