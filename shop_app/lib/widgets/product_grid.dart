fba2c4f79d3eb3d2c14b8a15cc4d3e7682606a46
tony <thaer.m.jafar@gmail.com>
2023-03-21 12:39:23 +0100

    use value provider in grid

--- shop_app/lib/widgets/product_grid.dart
+++ shop_app/lib/widgets/product_grid.dart
@@ -13,8 +13,8 @@ class ProductGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       itemCount: loadedProducts.length,
-      itemBuilder: (context, index) => ChangeNotifierProvider(
-        create: (context) => loadedProducts[index],
+      itemBuilder: (context, index) => ChangeNotifierProvider.value(
+        value: loadedProducts[index],
         child: const ProductItem(),
       ),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
