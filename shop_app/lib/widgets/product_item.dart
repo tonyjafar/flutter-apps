9444e9f28617427bdf7847abc271a2bef45c7408
tony <thaer.m.jafar@gmail.com>
2023-03-21 12:49:56 +0100

    use consumer

--- shop_app/lib/widgets/product_item.dart
+++ shop_app/lib/widgets/product_item.dart
@@ -8,7 +8,10 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(
+      context,
+      listen: false,
+    );
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -22,12 +25,14 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(
-              product.isFavorite ? Icons.favorite : Icons.favorite_border,
+          leading: Consumer<Product>(
+            builder: (ctx, product, child) => IconButton(
+              icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+              ),
+              onPressed: () => product.toggelFavoriteState(),
+              color: Theme.of(context).colorScheme.secondary,
             ),
-            onPressed: () => product.toggelFavoriteState(),
-            color: Theme.of(context).colorScheme.secondary,
           ),
           trailing: IconButton(
             icon: const Icon(Icons.shopping_cart),
